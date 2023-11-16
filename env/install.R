adf2f6c36f2e6c9518026cc12a1a89e8f4796d04
leonardHerault <leo.herault9338@gmail.com>
2023-11-16 15:12:01 +0100

    resolving installation issues

--- env/install.R
+++ env/install.R
@@ -1,2 +1,2 @@
-remotes::install_github("GfellerLab/SuperCell")
-remotes::install_github("rstudio/reticulate") #temporary fix for reading sparse matrix with R anndata https://github.com/rstudio/reticulate/issues/1417
+remotes::install_github("GfellerLab/SuperCell",upgrade = "never")
+remotes::install_github("rstudio/reticulate",upgrade = "never") #temporary fix for reading sparse matrix with R anndata https://github.com/rstudio/reticulate/issues/1417
