852f1b00df1f0254d0e71f869fee64d595164c3b
Mariia Bilous <38460599+mariiabilous@users.noreply.github.com>
2024-02-19 10:23:26 +0100

    fix: call assay in GetAssayData

--- R/mc_INV.R
+++ R/mc_INV.R
@@ -33,7 +33,7 @@ mc_INV <- function(sc.obj,
   
   sc.obj <- sc.obj[,names(membership_vector)]
 
-  INV_val <- stats::aggregate(Matrix::t(Seurat::GetAssayData(sc.obj, slot = "data")),
+  INV_val <- stats::aggregate(Matrix::t(Seurat::GetAssayData(sc.obj, assay = assay, slot = "data")),
                               by = list(metacell = membership_vector),
                               FUN = function(x) (1 / mean(x)) * var(x))
   rownames(INV_val) <- INV_val[, 1]
